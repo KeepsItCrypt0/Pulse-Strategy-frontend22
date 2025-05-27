90b1636af4a55ffb1ce529712c2ab1c8b698a44c
PulseStrategy <PLSstrategy@gmail.com>
2025-05-27 17:35:35 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -8,9 +7,6 @@ export default defineConfig({
     outDir: 'dist',
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      web3: path.resolve(__dirname, 'src/web3.js'), // Explicitly map "web3"
-    },
+    extensions: ['.js', '.jsx'], // Ensure .js files are recognized
   },
 });
