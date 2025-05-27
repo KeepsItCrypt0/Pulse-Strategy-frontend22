3b9078e0dc425b011ddd5e51e38cb6435c1a5d40
PulseStrategy <PLSstrategy@gmail.com>
2025-05-27 05:20:04 -0500

    Update PulseStrategyUserInfo.jsx

--- src/components/pulseStrategy/PulseStrategyUserInfo.jsx
+++ src/components/pulseStrategy/PulseStrategyUserInfo.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getTokenContract, formatNumber, networks } from "../../web3";
+import { getTokenContract, formatNumber, networks } from "./utils/format.js";
 
 const PulseStrategyUserInfo = ({ contract, account, web3 }) => {
   const [shareBalance, setShareBalance] = useState("0");
