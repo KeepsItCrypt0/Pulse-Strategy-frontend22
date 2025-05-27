64a0ddb808ea7f0999dd6fb72163eac3ffcffc97
PulseStrategy <PLSstrategy@gmail.com>
2025-05-27 05:31:26 -0500

    Update PulseStrategyRedeemPLSTR.jsx

--- src/components/pulseStrategy/PulseStrategyRedeemPLSTR.jsx
+++ src/components/pulseStrategy/PulseStrategyRedeemPLSTR.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { getTokenContract, formatNumber, networks } from "./utils/format.js";
+import { getTokenContract, formatNumber, networks } from "./utils/format";
 
 const PulseStrategyRedeemPLSTR = ({ contract, account, web3 }) => {
   const [amount, setAmount] = useState("");
