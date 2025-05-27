351b4a3c9c3faa38af781c20f9f33f4cb0db8d64
PulseStrategy <PLSstrategy@gmail.com>
2025-05-27 05:01:33 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { getWeb3, getContract, switchNetwork } from "./web3";
-import { networks, formatNumber, formatDate } from "../../utils/format";
+import { networks, formatNumber, formatDate } from "../utils/format"; // Updated path
 import PulseStrategyContractInfo from "./components/pulseStrategy/PulseStrategyContractInfo.jsx";
 import PulseStrategyIssuePLSTR from "./components/pulseStrategy/PulseStrategyIssuePLSTR.jsx";
 import PulseStrategyUserInfo from "./components/pulseStrategy/PulseStrategyUserInfo.jsx";
